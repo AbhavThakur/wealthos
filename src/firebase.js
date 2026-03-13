94a80ea2e13d0dc9f402adc7f4751139c1172289
Thakur, Abhav <Abhav.Thakur@bestbuy.com>
2026-03-13 15:30:24 +0530

    feat: add Firebase configuration and environment setup

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBT9tkjmv57c_N6jm8cVZvfr1D52fd6W7c",
-  authDomain: "wealthos-8fb50.firebaseapp.com",
-  projectId: "wealthos-8fb50",
-  storageBucket: "wealthos-8fb50.firebasestorage.app",
-  messagingSenderId: "901028887636",
-  appId: "1:901028887636:web:beada33fec3d7682dd01c1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
